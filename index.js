f685070332d67a168a372b0bfeffcb4ff7a91135
Sheldon <sheldon@SheldondeMacBook-Pro.local>
2017-03-09 16:04:30 +0800

    format

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
-const serve = require('koa-static');
-//const React = require('react');
-//const ReactDom = require('react-dom');
+const serve = require('koa-static');//静态资源
 
 //const  renderToString  = require('react-dom/server');直接输出组件HTML
 
-import createLogger from 'concurrency-logger';
-// import { Button } from 'antd';
+import createLogger from 'concurrency-logger';//format 日志
 import reactrouter from 'koa-react-router';
 import routes from './routes';
-import Container from './containers/Container'
+import Container from './containers/Container';
 
-// import HelloMessage from './index/index'
 
 const logger = createLogger(/* options */);
 
@@ -27,8 +23,6 @@ app.use(reactrouter({
     onNotFound: (ctx) => console.log('Not Found!!!'),
     onRender: (ctx) => ({Container}),
   }));
-
-
 
 app.listen(3000);
 console.log("http://127.0.0.1:3000")
